ae5db920661b23b4408c7ffa0dd52688e9833654
Simon-Duchatel Harmony <sd.harmony@yahoo.fr>
2020-02-02 11:52:31 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,19 +2,22 @@ public class Playeur{
 	private String nickname;
 	private int score;
 
+/* constructor*/
 	public Playeur(String n){
 		nickname=n;
 		score=0;
 
 	}
 
-
+/*getter for score*/
 	public int getscore(){
 		return score;
 	}
-	public String nickname(){
+/*getter for nickname*/
+	public String getNickname(){
 		return nickname;
 	}
+/*setter for score*/
 	public void setScore(int s){
 		score=s;
 	}
