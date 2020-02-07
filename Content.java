ae469c4930e4b182cbd4eab995b3f6c523fbb1ff
mayde24 <maya.douadi@outlook.fr>
2020-02-07 12:28:16 +0100

    Content class refound

--- Content.java
+++ Content.java
@@ -34,4 +34,39 @@ class Content {
     }
   }
 
+  // Class Character -----------------------------------------------
+  public Character {
+    private int x;
+    private int y;
+    private int bonus;
+
+    // ===================== Getter & Setter ========================
+    public int getX() { return this.x ; }
+    public void setX(int x) { this.x = x ; }
+
+    public int getY() { return this.y ; }
+    public void setY(int y) { this.y = y ; }
+
+    public int getBonus() { return this.bonus ; }
+    public void setBonus(int y) { this.bonus = bonus ; }
+
+    public void incrementBonus(int y) { this.setBonus(this.getBonus() + 1); }
+
+
+    // ===================== Constructor ========================
+    public Character(int t, int x, int y) {
+      super(t);
+      this.x = x;
+      this.y = y;
+      this.bonus = 0;
+    }
+
+    public Character(int t, int x, int y, int bonus) {
+      super(t);
+      this.x = x;
+      this.y = y;
+      this.bonus = bonus;
+    }
+  }
+
 }
