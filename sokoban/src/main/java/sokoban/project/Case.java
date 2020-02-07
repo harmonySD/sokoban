0a6f3c7e397138b8c040b74327606bf0fefb81dd
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2020-02-07 12:49:08 +0100

    refound Case.java

--- sokoban/src/main/java/sokoban/project/Case.java
+++ sokoban/src/main/java/sokoban/project/Case.java
@@ -1,41 +1,26 @@
 #import Content ;
 
 public class Case{
-  // coordonees de la case
-  // vraiment nécessaire si on fait juste un tableau de case ?!
-  private int x;
-  private int y;
-
-  private char isObjective;
-  // stocke couleur de l'objectif sous forme de Char et NULL si pas objectif
-  private char bonus;
-  // stocke type de bonus sous forme de Char et NULL si aucun
-  Content contain;
+  private String color;
   // le contenu de la case
+  private Content contain;
+  private boolean bonus;
+  
   // ============ Constructor ===========
-  public Case(int x, int y,char bonus,char isObjective){
-    this.x = x;
-    this.y = y;
-    this.bonus = bonus;
-    this.contain = NULL;
-    this.isObjective = isObjective;
-    // !! isObjective se choisit à la création !
+  public Case(String coul, Content cont, boolean b){
+    this.color = coul;
+    this.contain = cont;
+    this.bonus = b;
   }
 
 // ===================== Accesseurs & Mutateurs ========================
-// ------------ Coordonees ------------------------
-  public int getX() { return this.x ; }
-  public void setX(int x) { this.x = x ; }
+// ------------ Color ------------------------
+  public int getColor() { return this.color; }
+  public void setColor(String  s) { this.color = s; }
 
-  public int getY() { return this.y ; }
-  public void setY(int y) { this.y = y;}
-
-// ------------ Bonus et objectif ------------
+// ------------ Bonus ------------
   public char getBonus(){return this.bonus;}
   public void setBonus(char bonus){this.bonus = bonus;}
-
-  public char getObjective(){return this.isObjective};
-  // !! il est impossible de modifier le statut isObjective d'une case après sa création !
 
   // ------------ Contenu ----------------
   public Content getContent(){return this.contain;}
