b86679bc81eb816361aee53e4784e9d8603a4236
mayde24 <maya.douadi@outlook.fr>
2020-02-07 11:37:04 +0100

    Content variable updated & character deleted

--- Content.java
+++ Content.java
@@ -1,63 +1,35 @@
 class Content {
-  private int x;
-  private int y;
+
+  private int texture;
 
   // ===================== Getter & Setter ========================
-  public int getX() { return this.x ; }
-  public void setX(int num) { this.x = num ; }
-
-  public int getY() { return this.y ; }
-  public void setY(int num) { this.y = num ; }
+  public int getTexture() { return this.texture ; }
+  public void setTexture(int tmp) { this.texture = tmp ; }
 
   // ===================== Constructor ========================
-  public Content(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  // Class Character ------------------------------------------------
-  public Character {
-    private int texture;
-
-    // ===================== Getter & Setter ========================
-    public int getTexture() { return this.texture ; }
-    public void setTexture(int tmp) { this.texture = tmp ; }
-
-    // ===================== Constructor ========================
-    public Character(int t) {
-      this.texture = t;
-    }
+  public Content(int t) {
+    this.texture = t;
   }
 
   // Class Wall -----------------------------------------------------
   public Wall {
-    private int texture;
-
-    // ===================== Getter & Setter ========================
-    public int getTexture() { return this.texture ; }
-    public void setTexture(int tmp) { this.texture = tmp ; }
-
     // ===================== Constructor ========================
     public Wall(int t) {
-      this.texture = t;
+      super(t);
     }
   }
 
   // Class Box ------------------------------------------------------
   public Box {
-    private int texture;
     private string color;
 
     // ===================== Getter & Setter ========================
-    public int getTexture() { return this.texture ; }
-    public void setTexture(int tmp) { this.texture = tmp ; }
-
     public string getColor() { return this.color ; }
     public void setColor(string col) { this.color = col ; }
 
     // ===================== Constructor ========================
     public Box(int t, string c) {
-      this.texture = t;
+      super(t);
       this.color = c;
     }
   }
