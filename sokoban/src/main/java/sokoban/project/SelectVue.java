f2e3c067930746f5cf95c0aaf49fedf7ade0b4bb
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2020-05-10 18:35:31 +0200

    amelioration affichage niveaux

--- sokoban/src/main/java/sokoban/project/SelectVue.java
+++ sokoban/src/main/java/sokoban/project/SelectVue.java
@@ -25,44 +25,58 @@ import java.awt.*;
 
 
 public class SelectVue extends JFrame implements ActionListener{
-	private JButton b1= new JButton("ntest1");
-	private JButton b2= new JButton("ntest2");
+	private JButton [] button=new JButton[100000];
+	//private JButton b2= new JButton("ntest2");
 	private GameVue fenb1;
 	private String nom;
-	//private File levelPath = new File(System.getProperty("user.dir")+"/target/Niveaux"); 
-	//String [] listeDeNiveaux = levelPath.list();
-	//for(int i = 0; i < listeDeNiveaux.length;i++)System.out.println(listeDeNiveaux[i]);
+	private File levelPath = new File(System.getProperty("user.dir")+"/target/Niveaux"); 
+	private String [] listeDeNiveaux = levelPath.list();
+	int nbnivx=nbn(listeDeNiveaux);
+
+	public int nbn(String []l){
+		int nbn=0;
+		File levelPath = new File(System.getProperty("user.dir")+"/target/Niveaux"); 
+		String [] listeDeNiveaux = levelPath.list();
+		for(int i = 0; i < listeDeNiveaux.length;i++){
+			System.out.println(listeDeNiveaux[i]);
+			nbn++;
+		}
+		return nbn;
+	}
 	
 	public SelectVue(String n){
+		int nbniv=nbn(listeDeNiveaux);
 		this.nom=n;
-		this.setLayout(null);
+		this.setLayout(new GridLayout(nbniv,0,0,0));
+		for (int i=0; i<nbniv;i++ ) {
+			button[i]=new JButton(listeDeNiveaux[i]);
+			this.add(button[i]);
+
+			//button[i].addActionListener(this);
+		}
 
 		this.setTitle ("Sokoban select your level");
 		this.setSize(1000,1000);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
 
-		b1.setBounds(100,150,300,100);
-		this.add(b1);
-		b1.addActionListener(this);
+		//b1.setBounds(100,150,300,100);
+		//this.add(b1);
+		//b1.addActionListener(this);
 
-		b2.setBounds(100,250,300,100);
-		this.add(b2);
-		b2.addActionListener(this);
+		//b2.setBounds(100,250,300,100);
+		//this.add(b2);
+		//b2.addActionListener(this);
 		this.setVisible(true);
-	 File levelPath = new File(System.getProperty("user.dir")+"/target/Niveaux"); 
-	String [] listeDeNiveaux = levelPath.list();
-	for(int i = 0; i < listeDeNiveaux.length;i++)System.out.println(listeDeNiveaux[i]);
 	}
 
 
 
 	public void actionPerformed(ActionEvent arg0){
 		this.dispose();
-		if(arg0.getSource()==b1){
-			fenb1= new GameVue(nom,"niveautest.txt");
-		}
-		else if(arg0.getSource()== b2){
-			fenb1=new GameVue(nom,"niveautest2.txt");
+		for (int i=0;i<listeDeNiveaux.length ;i++ ) {
+			if (arg0.getSource()==button[i]) {
+				fenb1= new GameVue(nom,listeDeNiveaux[i]);
+			}
 		}
 		
 	}
